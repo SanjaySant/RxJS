ad29a9e0f2d5c9212e43492181279779867f4f99
SanjaySant <sanjaysant.2008@gmail.com>
2024-01-05 14:42:02 +0530

    RxJs compeleted

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PromiseComponent } from './promise/promise.component';
+import { ObservableComponent } from './observable/observable.component';
+
+const routes: Routes = [
+  { path: 'promise', component: PromiseComponent },
+  { path: 'observable', component: ObservableComponent },
+  { path: '**', redirectTo: 'promise' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
